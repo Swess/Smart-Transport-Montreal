6738139cbf3e7066041a2a3728a55098ef7026bf
Isaac Doré <swessmusic@gmail.com>
2019-01-19 14:14:52 -0500

    Working boilerplate for PB

---
+++ main.js
@@ -0,0 +1,15 @@
+'use strict';
+var protos = require('my-protos');
+
+
+let main = {
+
+    test: function(){
+        return "Testing";
+    }
+
+
+};
+
+console.log(main.test());
+console.log(protos);
